02027d0c56ada8b66ce2ea4c3d64da7cb5372ddd
chandmo123 <chandmo619@gmail.com>
2023-04-29 19:08:59 +0530

    task done

---
+++ prime.js
@@ -0,0 +1,16 @@
+function check_prime(num){
+    let count=0;
+    for(let i=0;i<num;i++){
+      if(num%i==0){
+        count++;
+      }
+    }
+    if(count==2){
+        console.log("prime");
+    }
+    else{
+        console.log("not prime");
+        }
+    }
+}
+check_prime(7);
